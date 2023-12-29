896b1d4cc39f87c2b9043ef1ee1ead136bd90eb4
Isaac Meléndez Negrete <124097164+isaacMelendezNegrete@users.noreply.github.com>
2023-12-29 02:47:05 -0600

    Se agregaron css y tsx para trabajar
    Según la estructura que entendí

--- src/Paginas/SkySatelital.tsx
+++ src/Paginas/SkySatelital.tsx
@@ -1,7 +1,30 @@
+import { useContext, useEffect, useState } from "react";
+import ContextFlujos from "../Context/ContextFlujos";
+
 const SkySatelital = () => {
+
+    const {setSkyPlus, setSkyCelular, setPais} = useContext(ContextFlujos) as any;
+
+  useEffect(() => {
+    if (sessionStorage.getItem("flujos") === "Sky+") {
+        setSkyPlus({estado:false});
+        setPais("MEX");
+      }else{
+        setPais(sessionStorage.getItem("pais"));
+      }
+
+      if (sessionStorage.getItem("flujos") ===  "DTH/SkyCelular" || sessionStorage.getItem("flujos") ===  "DTH/DTH/SkyCelular/Sky+") {
+        setSkyCelular({estado:true});
+      }
+
+
+  }, []);
 
     return (
         <>
+
         </>
     )
 }
+
+export default SkySatelital;
